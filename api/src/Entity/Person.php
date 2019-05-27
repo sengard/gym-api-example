6cd73454a4678b5ec0c16b36c9ddeea79dc829d5
Maxim <yalagin@gmial.com>
2019-05-27 17:38:14 +0300

    Add Person

---
+++ api/src/Entity/Person.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A person (alive, dead, undead, or fictional).
+ *
+ * @see http://schema.org/Person Documentation on Schema.org
+ *
+ * @ORM\Entity
+ * @ApiResource(iri="http://schema.org/Person")
+ */
+class Person
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="guid")
+     * @Assert\Uuid
+     */
+    private $id;
+
+    /**
+     * @var string|null the name of the item
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/name")
+     */
+    private $name;
+
+    /**
+     * @var string|null Family name. In the U.S., the last name of an Person. This can be used along with givenName instead of the name property.
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/familyName")
+     */
+    private $familyName;
+
+    /**
+     * @var string|null Given name. In the U.S., the first name of a Person. This can be used along with familyName instead of the name property.
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/givenName")
+     */
+    private $givenName;
+
+    /**
+     * @var string|null an additional name for a Person, can be used for a middle name
+     *
+     * @ORM\Column(type="text", nullable=true)
+     * @ApiProperty(iri="http://schema.org/additionalName")
+     */
+    private $additionalName;
+
+    /**
+     * @var PostalAddress|null physical address of the item
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\PostalAddress")
+     * @ApiProperty(iri="http://schema.org/address")
+     */
+    private $address;
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setFamilyName(?string $familyName): void
+    {
+        $this->familyName = $familyName;
+    }
+
+    public function getFamilyName(): ?string
+    {
+        return $this->familyName;
+    }
+
+    public function setGivenName(?string $givenName): void
+    {
+        $this->givenName = $givenName;
+    }
+
+    public function getGivenName(): ?string
+    {
+        return $this->givenName;
+    }
+
+    public function setAdditionalName(?string $additionalName): void
+    {
+        $this->additionalName = $additionalName;
+    }
+
+    public function getAdditionalName(): ?string
+    {
+        return $this->additionalName;
+    }
+
+    public function setAddress(?PostalAddress $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function getAddress(): ?PostalAddress
+    {
+        return $this->address;
+    }
+}
